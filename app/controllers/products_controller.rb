0ad43127aee3d4596448a6f2a8179b730ead66a8
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-01-30 21:36:48 +0100

    feat: add show details for a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,12 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
 end
