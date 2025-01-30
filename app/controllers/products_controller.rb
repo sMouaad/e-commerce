3764dbc4bdfbf1119e36f01600afedaecd33b59b
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-01-30 21:56:04 +0100

    feat: add logic to edit and create a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,39 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def show
     @product = Product.find(params[:id])
   end
 
   def new
     @product = Product.new
+  end
+
+  private
+
+  def product_params
+    params.expect(product: [ :name ])
   end
 end
